851d23c84820b1ef7957edbd50e7492bfae4a919
chengxiaolv <972987164@qq.com>
2018-11-16 17:40:28 +0800

    完成项目技术搭建

---
+++ src/components/homePage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+   
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+     s:10009993
+    }
+  },
+  methods:{
+
+  },
+  created(){
+    // console.log(this.utils.getNowTime());
+    // this.https.post("/news/index")
+    // .then((res)=>{
+    //   console.log(res)
+    // })
+    // .catch((res)=>{
+    //   console.log(res)
+    // })
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
